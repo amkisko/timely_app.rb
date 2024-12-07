ad2f2ef6d1f3a1c78ce19a9b7fd89675d92abb78
Andrei Makarov <andrei@kiskolabs.com>
2024-12-07 09:05:20 +0200

    Update spec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,30 +1,21 @@
-require_relative '../lib/timely-app'
-require 'webmock/rspec'
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter { |source_file| source_file.lines.count < 5 }
+end
 
-RSpec.shared_context 'TimelyApp::Client' do
-  let(:token) { 'token-xxx' }
-  let(:account_id) { 'account-id-xxx' }
-  let(:id) { 123 }
-  let(:ids) { %w[123 456 789] }
-  let(:base_url) { 'https://api.timelyapp.com' }
-  let(:auth_header) { {headers: {'Authorization' => "Bearer #{token}"}} }
-  let(:json_request) { {headers: {'Authorization' => "Bearer #{token}", 'Content-Type' => 'application/json'}, body: /\A{.+}\z/} }
-  let(:json_response_headers) { {'Content-Type' => 'application/json;charset=utf-8'} }
-  let(:json_response) { {headers: json_response_headers, body: '{}'} }
-  let(:json_array_response) { {headers: json_response_headers, body: '[]'} }
-  let(:client) { TimelyApp::Client.new(access_token: token, account_id: account_id) }
+require "simplecov-cobertura"
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
-  before do
-    WebMock.reset!
+# require "active_support"
+# require "active_support/json"
 
-    @request = nil
-  end
+require "webmock/rspec"
 
-  after do
-    expect(@request).to have_been_made.times(1) if @request
-  end
+require "timely-app"
 
-  def expect_request(*args)
-    @request = stub_request(*args)
-  end
+Dir[File.expand_path("support/**/*.rb", __dir__)].each { |f| require_relative f }
+
+RSpec.configure do |config|
+
 end
